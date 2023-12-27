a1477fb530ad2ca8da09fe9f46852913519cb552
Mustafa Burak Aydın <mburak.aydin@xenonsmart.com>
2023-12-27 16:22:47 +0300

    localhost:3000/graphql url'de çalışıyor.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { VideoModule } from './videos/video.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    VideoModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/videos/dto/video.dto.ts
@@ -0,0 +1,13 @@
+import { NewVideo } from 'src/graphql';
+import { IsNotEmpty } from 'class-validator';
+
+export class VideoDTO extends NewVideo {
+  @IsNotEmpty()
+  title: string;
+
+  @IsNotEmpty()
+  url: string;
+
+  @IsNotEmpty()
+  userId: string;
+}
